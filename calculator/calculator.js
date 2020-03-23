067712c9b7c6e2499fcb286aef52f69859da5fd4
feridun Akyol <feridunakyol34@gmail.com>
2020-03-23 13:01:51 +0100

    calculator

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,41 @@
-function add () {
-	
+function add(x, y) {
+	return x + y;
 }
 
-function subtract () {
-	
+function subtract(x, y) {
+	return x - y;
 }
 
-function sum () {
-	
+function sum(arr) {
+	let total = 0;
+	for (let i = 0; i < arr.length; i++) {
+		total = total + arr[i];
+	}
+	return total;
 }
 
-function multiply () {
-	
+function multiply(arr) {
+	let multi = 1;
+	for (let i = 0; i < arr.length; i++) {
+		multi = multi * arr[i];
+	}
+	return multi;
 }
 
-function power() {
-	
+function power(x, y) {
+	let power = 1;
+	for (let i = 0; i < y; i++) {
+		power = power * x;
+	}
+	return power;
 }
 
-function factorial() {
-	
+function factorial(x) {
+	let factor = 1;
+	for (let i = 1; i <= x; i++) {
+		factor = factor * i;
+	}
+	return factor;
 }
 
 module.exports = {
@@ -27,6 +43,6 @@ module.exports = {
 	subtract,
 	sum,
 	multiply,
-    power,
+	power,
 	factorial
-}
+};
