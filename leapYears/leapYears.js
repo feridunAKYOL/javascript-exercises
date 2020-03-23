f53644e1ea4ef5a7f512dc0313ad6e6281cdaea8
feridun Akyol <feridunakyol34@gmail.com>
2020-03-23 13:06:30 +0100

    leap years

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
-}
-
-module.exports = leapYears
+const leapYears = function(year) {
+	if (year % 400 === 0) {
+		return true;
+	}
+	if (year % 100 === 0) {
+		return false;
+	} else {
+		return year % 4 === 0;
+	}
+};
+module.exports = leapYears;
