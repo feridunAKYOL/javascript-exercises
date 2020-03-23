982419eae572c9fb9349e9f0c64d610e78379715
feridun Akyol <feridunakyol34@gmail.com>
2020-03-23 13:03:14 +0100

    fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+	let result = '';
+	if (!isNaN(num) && num > 0) {
+		let a = 1;
+		let b = 0;
+		for (let i = 1; i <= num; i++) {
+			result = a + b;
+			a = b;
+			b = result;
+		}
+	} else {
+		return 'OOPS';
+	}
 
-}
+	return result;
+};
 
-module.exports = fibonacci
+module.exports = fibonacci;
