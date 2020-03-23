fe9c623ad6d2768744bb4e0ed85d5bf7bf769a48
feridun Akyol <feridunakyol34@gmail.com>
2020-03-23 13:15:19 +0100

    temperature Conversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,12 +1,14 @@
-const ftoc = function() {
+const ftoc = function(f) {
+	let c = (f - 32) * 100 / 180;
+	return Number(c.toFixed(1));
+};
 
-}
-
-const ctof = function() {
-
-}
+const ctof = function(c) {
+	let f = c * 180 / 100 + 32;
+	return Number(f.toFixed(1));
+};
 
 module.exports = {
-  ftoc,
-  ctof
-}
+	ftoc,
+	ctof
+};
