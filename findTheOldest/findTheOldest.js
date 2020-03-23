ae8199774b55291acf8d0e9cd579e7f6c1c2eecd
feridun Akyol <feridunakyol34@gmail.com>
2020-03-23 13:04:41 +0100

    find the oldest

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-let findTheOldest = function() {
+let findTheOldest = function(arr) {
+	for (let item of arr) {
+		if (item.hasOwnProperty('yearOfDeath')) {
+			item.age = item.yearOfDeath - item.yearOfBirth;
+		} else {
+			item.age = 2020 - item.yearOfBirth;
+		}
+	}
+	let oldest = -Infinity;
+	let name = {};
+	for (let item of arr) {
+		if (item.age > oldest) {
+			oldest = item.age;
+			name = item;
+		}
+	}
+	return name;
+};
 
-}
-
-module.exports = findTheOldest
+module.exports = findTheOldest;
